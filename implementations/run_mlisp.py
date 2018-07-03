8f1ddbfdd5f014e9a8f1d0c753df5cecf159995f
thautwarm <twshere@outlook.com>
2018-07-03 15:45:02 +0800

    llvm-lisp, mlisp: abstract machine not finished

--- implementations/run_mlisp.py
+++ implementations/run_mlisp.py
@@ -5,11 +5,17 @@ ze_exp = ze.compile('import mlisp.mlisp.[*]', use='grammar')
 s = ze_exp.match("""
 (def f 
     (lambda (x, y) 
-            (+ x y))
-)
+            (+ x y)))
 
 (let s 1)
 (let f 1.0)
+
+(def Point 
+    (struct
+        (x:i32 y:i32)))
+        
 """)
 
+for each in s.tokens:
+    print(each)
 print(s.result)
